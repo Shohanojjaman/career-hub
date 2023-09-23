ed47dc8bf5a907e51c538fcaa790e8e6d768e561
Shohanojjaman <shohanojjamanemon@gmail.com>
2023-09-23 23:13:28 +0600

    Fix some issue.

--- src/components/Utilities/Header/Header.jsx
+++ src/components/Utilities/Header/Header.jsx
@@ -48,8 +48,8 @@ const Header = () => {
   return (
     <header className="container mx-auto max-sm:px-5 navbar bg-base-100">
       <div className="navbar-start">
-        <details className="dropdown">
-          <summary  className="btn btn-ghost lg:hidden">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-5 w-5"
@@ -58,15 +58,17 @@ const Header = () => {
               stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
             </svg>
-          </summary>
-          <ul  className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+          </label>
+          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
             {links}
           </ul>
-        </details>
+        </div>
         <a className="text-2xl md:text-4xl text-heading font-extrabold">CareerHub</a>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">{links}</ul>
+        <ul tabIndex={0} className="menu menu-horizontal px-1">
+          {links}
+        </ul>
       </div>
       <div className="navbar-end">
         <Link className="btn theme-bg text-white">Star Applying</Link>
