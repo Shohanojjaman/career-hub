17c14d6f1fbfaade833bc091e75f4526ae36a0c0
Shohanojjaman <shohanojjamanemon@gmail.com>
2023-09-24 11:31:53 +0600

    Added Banner and Job Cetagory section.

--- src/components/pages/Statistics/Statistics.jsx
+++ src/components/pages/Statistics/Statistics.jsx
@@ -1,8 +1,12 @@
-import Banner from './Sections/Banner'
+import Banner from './Sections/Banner';
+import JobCategory from './Sections/JobCategory';
 const Statistics = () => {
-  return <div className="">
-    <Banner></Banner>
-  </div>;
+  return (
+    <div className="">
+      <Banner></Banner>
+      <JobCategory></JobCategory>
+    </div>
+  );
 };
 
 export default Statistics;
