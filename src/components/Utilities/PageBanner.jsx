bd938150b76cc4c96a46649c7a6ff7bd40c882c5
Shohanojjaman <shohanojjamanemon@gmail.com>
2023-09-25 12:16:35 +0600

    Created Dynamic page banner

---
+++ src/components/Utilities/PageBanner.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+import Header from './Header/Header';
+
+const PageBanner = ({ title }) => {
+  return <section className="banner-bg h-96">
+    <Header></Header>
+    <div className="h-4/5 flex justify-center items-center">
+        <h1 className='text-4xl font-extrabold text-heading'>{title}</h1>
+    </div>
+  </section>;
+};
+
+PageBanner.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+export default PageBanner;
