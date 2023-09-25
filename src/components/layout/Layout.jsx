5358c0c876f2db77baeca2212aca421fd0062978
Shohanojjaman <shohanojjamanemon@gmail.com>
2023-09-25 11:54:52 +0600

    Move Header to Banner.

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,11 +1,9 @@
 import { Outlet } from 'react-router-dom';
 import Footer from '../Utilities/Footer/Footer';
-import Header from '../Utilities/Header/Header';
 
 const Layout = () => {
   return (
     <>
-      <Header></Header>
       <main>
         <Outlet></Outlet>
       </main>
